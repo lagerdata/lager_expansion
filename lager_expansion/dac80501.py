d3dded11d0789e6fb3809357f9a7c4cbfa3f694f
Max Henstell <git@kapamaki.net>
2021-10-15 09:58:56 -0700

    Convert to spaces

--- lager_expansion/dac80501.py
+++ lager_expansion/dac80501.py
@@ -12,50 +12,50 @@ DAC_REG_DAC = 0x08
 DAC_ADDR = 0x48
 
 class DAC80501:
-	def __init__(self, device, address=DAC_ADDR):
-		self.device = device
-		self.address = address
-		self.divider = False
-		self.buffer_gain = False
+    def __init__(self, device, address=DAC_ADDR):
+        self.device = device
+        self.address = address
+        self.divider = False
+        self.buffer_gain = False
 
-	def begin(self):
-		self.reset()
-		time.sleep(0.01)
+    def begin(self):
+        self.reset()
+        time.sleep(0.01)
 
-	def reset(self):
-		self.device.write(self.address, Flags.FLAG_START_STOP, [DAC_REG_TRIGGER, 0x00, 0x0A])
+    def reset(self):
+        self.device.write(self.address, Flags.FLAG_START_STOP, [DAC_REG_TRIGGER, 0x00, 0x0A])
 
-	def get_alarm(self):
-		status = self.device.read(self.address, DAC_REG_STATUS, 2)
-		return status[1] & 0x01 == True
+    def get_alarm(self):
+        status = self.device.read(self.address, DAC_REG_STATUS, 2)
+        return status[1] & 0x01 == True
 
-	def set_gain(self, divider, buffer_gain):
-		self.divider = divider
-		self.buffer_gain = buffer_gain
+    def set_gain(self, divider, buffer_gain):
+        self.divider = divider
+        self.buffer_gain = buffer_gain
 
-		div = 0x01 if divider is True else 0x00
-		buf = 0x01 if buffer_gain is True else 0x00
+        div = 0x01 if divider is True else 0x00
+        buf = 0x01 if buffer_gain is True else 0x00
 
-		self.device.write(self.address, Flags.FLAG_START_STOP, [DAC_REG_GAIN, div, buf])
+        self.device.write(self.address, Flags.FLAG_START_STOP, [DAC_REG_GAIN, div, buf])
 
-	def set_config(self, reference, power_down):
-		ref = 0x00 if reference is True else 0x01
-		pwr = 0x01 if power_down is True else 0x00
-		self.device.write(self.address, Flags.FLAG_START_STOP, [DAC_REG_CONFIG, ref, pwr])
+    def set_config(self, reference, power_down):
+        ref = 0x00 if reference is True else 0x01
+        pwr = 0x01 if power_down is True else 0x00
+        self.device.write(self.address, Flags.FLAG_START_STOP, [DAC_REG_CONFIG, ref, pwr])
 
-	def set_value(self, value):
-		self.device.write(self.address, Flags.FLAG_START_STOP, [DAC_REG_DAC, (value >> 8) & 0xFF, value & 0xFF])
-		# self.device.read(self.address, DAC_REG_DAC, 2)
-	
-	def set_volts(self, volts):
+    def set_value(self, value):
+        self.device.write(self.address, Flags.FLAG_START_STOP, [DAC_REG_DAC, (value >> 8) & 0xFF, value & 0xFF])
+        # self.device.read(self.address, DAC_REG_DAC, 2)
+    
+    def set_volts(self, volts):
 
-		volts = 5 if volts > 5 else volts
-		volts = 0 if volts < 0 else volts
+        volts = 5 if volts > 5 else volts
+        volts = 0 if volts < 0 else volts
 
-		vrefio = 2.5
-		div = 1 if self.divider is False else 2
-		gain = 2 if self.buffer_gain else 1
+        vrefio = 2.5
+        div = 1 if self.divider is False else 2
+        gain = 2 if self.buffer_gain else 1
 
-		dac_data = int(((volts / gain) / (vrefio / div)) * (2**16 - 1))
-		print(f"Data: {dac_data} Volts: {volts}")
-		self.device.write(self.address, Flags.FLAG_START_STOP, [DAC_REG_DAC, (dac_data >> 8) & 0xFF, dac_data & 0xFF])
+        dac_data = int(((volts / gain) / (vrefio / div)) * (2**16 - 1))
+        print(f"Data: {dac_data} Volts: {volts}")
+        self.device.write(self.address, Flags.FLAG_START_STOP, [DAC_REG_DAC, (dac_data >> 8) & 0xFF, dac_data & 0xFF])
--- lager_expansion/pca9685.py
+++ lager_expansion/pca9685.py
@@ -14,45 +14,45 @@ PCA9685_LED0_ON_L = 0x06
 FREQUENCY_OSCILLATOR = 25000000
 
 class OutputModes(Enum):
-	PCA_PUSH_PULL = 0
-	PCA_OPEN_DRAIN = 1
+    PCA_PUSH_PULL = 0
+    PCA_OPEN_DRAIN = 1
 
 class PCA9685:
-	def __init__(self, device, address):
-		self.device = device
-		self.address = address
+    def __init__(self, device, address):
+        self.device = device
+        self.address = address
 
-	def begin(self):
-		self.reset()
+    def begin(self):
+        self.reset()
 
-	def reset(self):
-		self.device.write(self.address, Flags.FLAG_START_STOP, [PCA9685_MODE1, MODE1_RESTART])
-		time.sleep(0.01)
+    def reset(self):
+        self.device.write(self.address, Flags.FLAG_START_STOP, [PCA9685_MODE1, MODE1_RESTART])
+        time.sleep(0.01)
 
-	def set_pwm_freq(self, freq):
-		prescale =  int((FREQUENCY_OSCILLATOR / (freq * 4096.0)) + 0.5)
+    def set_pwm_freq(self, freq):
+        prescale =  int((FREQUENCY_OSCILLATOR / (freq * 4096.0)) + 0.5)
 
-		oldmode = 0x00 # todo read
-		newmode = (oldmode & ~MODE1_RESTART) | MODE1_SLEEP
-		self.device.write(self.address, Flags.FLAG_START_STOP, [PCA9685_MODE1, newmode])
-		self.device.write(self.address, Flags.FLAG_START_STOP, [PCA9685_PRESCALE, prescale])
-		self.device.write(self.address, Flags.FLAG_START_STOP, [PCA9685_MODE1, oldmode])
-		time.sleep(0.01)
-		self.device.write(self.address, Flags.FLAG_START_STOP, [PCA9685_MODE1, oldmode | MODE1_RESTART | MODE1_AI])
-		time.sleep(0.01)
+        oldmode = 0x00 # todo read
+        newmode = (oldmode & ~MODE1_RESTART) | MODE1_SLEEP
+        self.device.write(self.address, Flags.FLAG_START_STOP, [PCA9685_MODE1, newmode])
+        self.device.write(self.address, Flags.FLAG_START_STOP, [PCA9685_PRESCALE, prescale])
+        self.device.write(self.address, Flags.FLAG_START_STOP, [PCA9685_MODE1, oldmode])
+        time.sleep(0.01)
+        self.device.write(self.address, Flags.FLAG_START_STOP, [PCA9685_MODE1, oldmode | MODE1_RESTART | MODE1_AI])
+        time.sleep(0.01)
 
-	def set_output(self, mode):
-		if mode == OutputModes.PCA_PUSH_PULL:
-			# todo read mode
-			self.device.write(self.address, Flags.FLAG_START_STOP, [PCA9685_MODE2, 0x00 | MODE2_OUTDRV])
+    def set_output(self, mode):
+        if mode == OutputModes.PCA_PUSH_PULL:
+            # todo read mode
+            self.device.write(self.address, Flags.FLAG_START_STOP, [PCA9685_MODE2, 0x00 | MODE2_OUTDRV])
 
-	def set_pwm_precise(self, num, on, off):
-		self.device.write(self.address, Flags.FLAG_START_STOP, [PCA9685_LED0_ON_L + 4 * num, on & 0xFF, (on >> 8) & 0xFF , off & 0xFF, (off >> 8) & 0xFF])
+    def set_pwm_precise(self, num, on, off):
+        self.device.write(self.address, Flags.FLAG_START_STOP, [PCA9685_LED0_ON_L + 4 * num, on & 0xFF, (on >> 8) & 0xFF , off & 0xFF, (off >> 8) & 0xFF])
 
-	def set_pwm(self, num, value):
-		if value >= 4095:
-			self.set_pwm_precise(num, 4096, 0)
-		elif value == 0:
-			self.set_pwm_precise(num, 0, 4096)
-		else:
-			self.set_pwm_precise(num, 0, value)
+    def set_pwm(self, num, value):
+        if value >= 4095:
+            self.set_pwm_precise(num, 4096, 0)
+        elif value == 0:
+            self.set_pwm_precise(num, 0, 4096)
+        else:
+            self.set_pwm_precise(num, 0, value)
